e10ad60e46e2b26f868e88e280b7216adb87f2a0
edisonkastratii <edison.kastrati04@gmail.com>
2023-05-15 22:17:18 +0200

    adding constructor and getters setters in review

--- src/main/java/com/edisonkastrati/bookapp/entity/Review.java
+++ src/main/java/com/edisonkastrati/bookapp/entity/Review.java
@@ -25,4 +25,63 @@ public class Review {
     private long bookId;
 
     private String reviewDescription;
+
+    public Review() {
+
+    }
+    public Review(String userEmail, Date date, double rating, long bookId, String reviewDescription) {
+        this.userEmail = userEmail;
+        this.date = date;
+        this.rating = rating;
+        this.bookId = bookId;
+        this.reviewDescription = reviewDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getReviewDescription() {
+        return reviewDescription;
+    }
+
+    public void setReviewDescription(String reviewDescription) {
+        this.reviewDescription = reviewDescription;
+    }
 }
