8c450f17abc5850825becd651c23e62deaa863d8
edisonkastratii <edison.kastrati04@gmail.com>
2023-06-12 14:48:42 +0200

    find message by user email and disable http

--- src/main/java/com/edisonkastrati/bookapp/config/MyDataRestConfig.java
+++ src/main/java/com/edisonkastrati/bookapp/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.edisonkastrati.bookapp.config;
 
 import com.edisonkastrati.bookapp.entity.Book;
+import com.edisonkastrati.bookapp.entity.Message;
 import com.edisonkastrati.bookapp.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -24,9 +25,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(Message.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
         disableHttpMethods(Review.class, config, theUnsupportedActions);
+        disableHttpMethods(Message.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
